d37e459ed83088a8be4fbdfbb4188fc9fad318cf
WHY <941443383@qq.com>
2022-05-02 22:40:36 +0800

    解决了访问不到index.html的问题

--- src/main/java/com/why/plant/controller/indexController.java
+++ src/main/java/com/why/plant/controller/indexController.java
@@ -4,17 +4,19 @@ package com.why.plant.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 
-@Controller
+@RestController
 public class indexController {
 
     @RequestMapping("/index")
     public ModelAndView indexHtml(HashMap<String, Object> map) {
-        ModelAndView mv =  new ModelAndView("index");
+        ModelAndView mv =  new ModelAndView("/index");
         return mv;
     }
+
 
 }
